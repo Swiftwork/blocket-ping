bfd0da804fe2a77aaf5ab995b696977e57774ce5
Erik Hughes <erik.hughes@outlook.com>
2019-09-21 11:38:55 +0200

    fix: skip deploy tag warning on success

--- deploy.cmd
+++ deploy.cmd
@@ -8,7 +8,10 @@ docker login sldevrg.azurecr.io
 docker build -t sldevrg.azurecr.io/erik.blocket-ping:latest -t sldevrg.azurecr.io/erik.blocket-ping:%1 .
 docker push sldevrg.azurecr.io/erik.blocket-ping:latest
 docker push sldevrg.azurecr.io/erik.blocket-ping:%1
+GOTO DONE
 
 :BLANK
 
 ECHO Please supply a tag
+
+:DONE
